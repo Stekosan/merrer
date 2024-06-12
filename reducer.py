9c9c23a6645b16131fdfd2455af8e9e6e27e2f40
Stéphane Berhault <144647002+Stekosan@users.noreply.github.com>
2024-06-12 11:33:55 -0700

    Update reducer.py
    update v2

--- reducer.py
+++ reducer.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
-  
-from operator import itemgetter 
-import sys 
-  
-current_word = None
-current_count = 0
-word = None
 
-for line in sys.stdin: 
-    line = line.strip() 
-    word, count = line.split('\t', 1) 
-    try: 
-        count = int(count) 
-    except ValueError: 
+# ---------------------------------------------------
+# Script Name: reducer.py
+# Date: June 12, 2024
+# Purpose: This script serves as the reducer function in a MapReduce job. It takes the output
+#          from the mapper function (key-value pairs in the format (word, 1)), aggregates the
+#         counts for each word, and outputs the word-count pairs in the format 'word count'.
+# Author: Stephane Berhault
+# ---------------------------------------------------
+
+from operator import itemgetter
+import sys
+
+word_counts = {}
+
+# Read input from stdin
+for line in sys.stdin:
+    line = line.strip()
+    word, count = line.split('\t', 1)
+    try:
+        count = int(count)
+        word_counts[word] = word_counts.get(word, 0) + count
+    except ValueError:
         continue
-    if current_word == word: 
-        current_count += count 
-    else: 
-        if current_word: 
-            print('%s\t%s' % (current_word, current_count))
-        current_count = count 
-        current_word = word 
-if current_word == word: 
-    print('%s\t%s' % (current_word, current_count))
+
+# Sort word-count pairs by word in alphabetical order
+sorted_word_counts = sorted(word_counts.items(), key=itemgetter(0))
+
+# Print word-count pairs in the desired format
+for word, count in sorted_word_counts:
+    print(f"{word} {count}")
