91aff2cc2bc584cd05bcda81bade8b5d8bdef720
Stéphane Berhault <144647002+Stekosan@users.noreply.github.com>
2024-06-19 14:06:58 -0700

    Update make_tsv.py

--- make_tsv.py
+++ make_tsv.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*
+
+# ----------------------------------------------------------
+# Script Name: make_tsv.py
+# Date: June 19, 2024
+# Purpose: This script serves as the mapper function in a MapReduce job. It takes input text data,
+#         creates a Google-compliant .tsv file with url, file size, and md5 checksum.
+# Author: Stephane Berhault
+# ----------------------------------------------------------
+
+
+
 import os, hashlib, base64
 
 
